02fed59ae6c20aeb0f8016d4d45c37a1afce1b42
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-31 08:16:25 +0200

    Feat: change name minimum character to 2
    * Names with 2 characters are possible

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true, minLength: 3, maxLength: 50 },
-  last_name: { type: String, required: true, minLength: 3, maxLength: 50 },
+  first_name: { type: String, required: true, minLength: 2, maxLength: 50 },
+  last_name: { type: String, required: true, minLength: 2, maxLength: 50 },
   username: { type: String, required: true, minLength: 5, maxLength: 20 },
   membership_status: { type: Boolean, required: true },
   password: { type: String, required: true, minLength: 10, maxLength: 100 },
